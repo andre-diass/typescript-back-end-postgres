2e35273db8360a08a60379ec6d769aab2b2e9da6
andre-diass <adsf@poli.br>
2023-08-24 22:54:08 -0300

    build: setup server

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("server up 2");
+});
+
+app.listen(PORT, () => {
+  console.log(`server running in port  ${PORT}`);
+});
