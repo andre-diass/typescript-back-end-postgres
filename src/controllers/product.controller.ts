49626ca236303c849fe69336eccfaf0e9283068a
andre-diass <adsf@poli.br>
2023-08-29 16:03:50 -0300

    refactor: apply SRP (S.O.L.I.D) and encapsulate func.

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from "express";
 import AppDataSource from "../connection";
 import { Product } from "../entities/product.entity";
+import { Repository } from "typeorm";
 
 class productController {
-  async findAll(req: Request, res: Response): Promise<Response> {
-    const productRepository = AppDataSource.getRepository(Product);
+  private productRepository: Repository<Product>;
 
-    const products = await productRepository.find();
+  constructor() {
+    this.productRepository = AppDataSource.getRepository(Product);
+  }
+
+  async findAll(req: Request, res: Response): Promise<Response> {
+    //const productRepository = AppDataSource.getRepository(Product);
+
+    const products = await this.productRepository.find();
 
     return res.status(200).send({ data: [products] });
   }
