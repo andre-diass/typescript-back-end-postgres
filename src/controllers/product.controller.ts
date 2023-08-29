9be72148d354520a6a6e593120e8e52ff92c8347
andre-diass <adsf@poli.br>
2023-08-29 17:06:29 -0300

    fix: ES6 syntax

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -10,26 +10,26 @@ class productController {
     this.productRepository = AppDataSource.getRepository(Product);
   }
 
-  async findAll(req: Request, res: Response): Promise<Response> {
-    const productRepository = AppDataSource.getRepository(Product);
+  findAll = async (req: Request, res: Response): Promise<Response> => {
+    //const productRepository = AppDataSource.getRepository(Product);
 
-    const products = await productRepository.find();
+    const products = await this.productRepository.find();
 
     return res.status(200).send({ data: [products] });
-  }
+  };
 
-  async create(req: Request, res: Response): Promise<Response> {
+  create = async (req: Request, res: Response): Promise<Response> => {
     const { name, description } = req.body;
 
     const product = new Product();
     product.name = name;
     product.description = description;
 
-    const productRepository = await AppDataSource.getRepository(Product);
-    const productDb = await productRepository.save(product);
+    //const productRepository = await AppDataSource.getRepository(Product);
+    const productDb = await this.productRepository.save(product);
 
     return res.status(201).send({ data: productDb });
-  }
+  };
 }
 
 export default new productController();
